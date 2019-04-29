45b65466a4a1fb886cda795cae5d31c3ae7975c4
anna.kashubina <anna.kashubina@osinit.com>
2019-04-29 13:58:40 +0300

    Update codestyle

--- lecture_1/bikes/bikes_site/tests.py
+++ lecture_1/bikes/bikes_site/tests.py
@@ -8,35 +8,35 @@ from .models import Category, Company, Motobike
 
 @pytest.fixture
 def setup(transactional_db):
-    Category.objects.create(name="Мотоциклы")
-    Category.objects.create(name="Мопеды")
-    Category.objects.create(name="Квадроциклы")
+    Category.objects.create(name='Мотоциклы')
+    Category.objects.create(name='Мопеды')
+    Category.objects.create(name='Квадроциклы')
 
-    Company.objects.create(name="Kawasaki")
-    Company.objects.create(name="Honda")
+    Company.objects.create(name='Kawasaki')
+    Company.objects.create(name='Honda')
 
     Motobike.objects.create(
-        company=Company.objects.get(name="Honda"),
-        category=Category.objects.get(name="Мопеды"),
-        name="Giorno",
+        company=Company.objects.get(name='Honda'),
+        category=Category.objects.get(name='Мопеды'),
+        name='Giorno',
     )
 
     Motobike.objects.create(
-        company=Company.objects.get(name="Kawasaki"),
-        category=Category.objects.get(name="Мотоциклы"),
-        name="Ninja",
+        company=Company.objects.get(name='Kawasaki'),
+        category=Category.objects.get(name='Мотоциклы'),
+        name='Ninja',
     )
 
     Motobike.objects.create(
-        company=Company.objects.get(name="Kawasaki"),
-        category=Category.objects.get(name="Мотоциклы"),
-        name="Ninja Turbo",
+        company=Company.objects.get(name='Kawasaki'),
+        category=Category.objects.get(name='Мотоциклы'),
+        name='Ninja Turbo',
     )
 
 
 def test_models(setup):
-    assert Company.objects.get(motobike__name="Giorno").name == "Honda"
-    assert Category.objects.get(motobike__name="Ninja").name == "Мотоциклы"
+    assert Company.objects.get(motobike__name='Giorno').name == 'Honda'
+    assert Category.objects.get(motobike__name='Ninja').name == 'Мотоциклы'
 
 
 def test_index():
@@ -50,20 +50,20 @@ def test_categories(setup):
     client = Client()
     response = client.get('/categories/')
     assert response.status_code == 200
-    assert json.loads(response.content.decode('utf-8'))[2]["name"] == 'Квадроциклы'
+    assert json.loads(response.content.decode('utf-8'))[2]['name'] == 'Квадроциклы'
 
 
 def test_category(setup):
     client = Client()
-    category_id = Category.objects.get(name="Мотоциклы").id
+    category_id = Category.objects.get(name='Мотоциклы').id
     response = client.get(f'/categories/{category_id}/')
     assert response.status_code == 200
     response_data = json.loads(response.content.decode('utf-8'))
     assert len(response_data) == 2
-    assert response_data[1]["name"] == 'Ninja Turbo'
-    assert response_data[1]["vendor"] == 'Kawasaki'
-    assert response_data[1]["category"] == 'Мотоциклы'
-    assert response_data[1]["description"] == ''
+    assert response_data[1]['name'] == 'Ninja Turbo'
+    assert response_data[1]['vendor'] == 'Kawasaki'
+    assert response_data[1]['category'] == 'Мотоциклы'
+    assert response_data[1]['description'] == ''
 
     response = client.get(f'/categories/25/')
     assert response.status_code == 404
@@ -71,16 +71,16 @@ def test_category(setup):
 
 def test_details(setup):
     client = Client()
-    motobike_id = Motobike.objects.get(name="Ninja").id
+    motobike_id = Motobike.objects.get(name='Ninja').id
     response = client.get(f'/details/{motobike_id}/')
 
     assert response.status_code == 200
 
     response_data = json.loads(response.content.decode('utf-8'))
-    assert response_data["name"] == 'Ninja'
-    assert response_data["vendor"] == 'Kawasaki'
-    assert response_data["category"] == 'Мотоциклы'
-    assert response_data["description"] == ''
+    assert response_data['name'] == 'Ninja'
+    assert response_data['vendor'] == 'Kawasaki'
+    assert response_data['category'] == 'Мотоциклы'
+    assert response_data['description'] == ''
 
     response = client.get(f'/details/25/')
     assert response.status_code == 404
