5048656d0aecc9bed40b250496252b99d5c61b84
polina.novokreschenova <noverianhenon@yandex.ru>
2019-05-01 21:40:54 +0300

    #33204: update pytest config

--- lecture_1/bikes/bikes_site/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
-from .models import Category, Motobike
-import json
-from django.shortcuts import get_object_or_404
-
-
-# Создайте представления, исходя из urls
