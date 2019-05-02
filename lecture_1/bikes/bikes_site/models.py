a5ea9cc948bc1e31fd889055faa6c68de84538f4
polina.novokreschenova <noverianhenon@yandex.ru>
2019-05-02 16:15:42 +0300

    #33204: modify models

--- lecture_1/bikes/bikes_site/models.py
+++ lecture_1/bikes/bikes_site/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
@@ -17,12 +17,8 @@ class Company(models.Model):
 
 class Motobike(models.Model):
     name = models.CharField(max_length=200)
-    category = models.ForeignKey(
-        Category, db_column='category', on_delete=models.CASCADE, to_field='name'
-    )
-    company = models.ForeignKey(
-        Company, db_column='company', on_delete=models.CASCADE, to_field='name'
-    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
     description = models.TextField(blank=True)
 
     def __str__(self):
