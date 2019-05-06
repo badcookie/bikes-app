9cbc7dc9364b72e5bf314b28fbbd5fedfb6a8307
Sergei Emelianov <karagabul@gmail.com>
2019-05-06 15:07:52 +0300

    Добавил создание базового issue для задания, чтобы по каждому можно отдельно смотреть

--- managment/main.py
+++ managment/main.py
@@ -18,13 +18,23 @@ def create_issues(issue_id):
     ]
     Список задач должен следующего формата:
     issues = [
-        {'subject': 'Задача', 'start_date': datetime.date(2019, 4, 29)},
+        {'subject': 'Задача', 'start_date': datetime.date(2019, 4, 29), assign_to: 130},
     ]
     :param issue_id: индекс в списке задач
     :return:
     """
     issue = issues[issue_id]
+    assign_to = issue.pop('assign_to')
     due_date = issue['start_date'] + datetime.timedelta(days=7)
+    base_issue = redmine.issue.create(
+        project_id=project_id,
+        tracker_id=tracker_id,
+        status_id=status_id,
+        priority_id=priority_id,
+        assigned_to_id=assign_to,
+        due_date=due_date,
+        **issue,
+    )
     for user in users:
         # TODO: сделать проверку, что такого задания еще нет.
         try:
@@ -34,8 +44,10 @@ def create_issues(issue_id):
                 status_id=status_id,
                 priority_id=priority_id,
                 assigned_to_id=user['id'],
+                parent_issue_id=base_issue.id,
                 due_date=due_date,
-                **issue,
+                start_date=issue['start_date'],
+                subject='{}: {}'.format(issue['subject'], user['name'])
             )
         except Exception as e:
             # Наш redmine отдает ответ с ошибкой, поэтому просто игнорируем
