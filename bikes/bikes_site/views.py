0eba0daa6e2ae5f788b9da38ebd15a7479f3713c
polina.novokreschenova <noverianhenon@yandex.ru>
2019-05-10 07:48:29 +0300

    #33367: add docstrings in viewsets

--- bikes/bikes_site/views.py
+++ bikes/bikes_site/views.py
@@ -17,21 +17,57 @@ class PublicProductListPagination(PageNumberPagination):
 
 
 class ProductViewSet(viewsets.ModelViewSet):
+    """Defines actions and queries accessible to managers only."""
+
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = ProductSerializer
 
     def get_queryset(self):
+        """Collects all products for manager's company.
+
+        Returns
+        -------
+            Products queryset.
+
+        """
+
         manager = get_object_or_404(Manager, user=self.request.user)
         products = Product.objects.filter(company=manager.company)
         return products
 
     def perform_create(self, serializer):
+        """Creates a product instance from manager's company.
+
+        Parameters
+        ----------
+        serializer
+
+        Returns
+        -------
+            Product instance with valid data.
+
+        """
+
         category_id = self.request.data['category']
         category = get_object_or_404(Category, id=category_id)
         manager = get_object_or_404(Manager, user=self.request.user)
         serializer.save(company=manager.company, category=category)
 
     def update(self, request, *args, **kwargs):
+        """Modifies product instance fully or partially.
+
+        Parameters
+        ----------
+        request
+        args
+        kwargs
+
+        Returns
+        -------
+            Product's modified data.
+
+        """
+
         product = get_object_or_404(Product, id=kwargs['pk'])
         manager = get_object_or_404(Manager, user=request.user)
         if manager.company != product.company:
@@ -46,6 +82,20 @@ class ProductViewSet(viewsets.ModelViewSet):
 
     @action(methods=['get'], detail=False, url_path='category/(?P<pk>[^/.]+)')
     def category(self, request, pk):
+        """Nested route to get list of company's products for a particular category.
+
+        Parameters
+        ----------
+        request
+        pk
+            Category id.
+
+        Returns
+        -------
+            Products list.
+
+        """
+
         category = get_object_or_404(Category, id=pk)
         category_products = self.get_queryset().filter(category=category)
         category_products_list = [
@@ -55,6 +105,8 @@ class ProductViewSet(viewsets.ModelViewSet):
 
 
 class PublicProductViewSet(viewsets.ModelViewSet):
+    """Defines actions and queries accessible to all users."""
+
     http_method_names = ['get']
     permission_classes = (permissions.AllowAny,)
     serializer_class = PublicProductSerializer
@@ -63,6 +115,20 @@ class PublicProductViewSet(viewsets.ModelViewSet):
 
     @action(methods=['get'], detail=False, url_path='category/(?P<pk>[^/.]+)')
     def category(self, request, pk):
+        """Nested route to get list of all products for a particular category.
+
+        Parameters
+        ----------
+        request
+        pk
+            Category id.
+
+        Returns
+        -------
+            Paginated products list.
+
+        """
+
         category = get_object_or_404(Category, id=pk)
         category_products = self.get_queryset().filter(category=category)
         products_per_page = self.paginate_queryset(category_products)
