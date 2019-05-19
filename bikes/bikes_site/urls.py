14b96f99c836a40c6476fe697ede38a89d42d47a
polina.novokreschenova <noverianhenon@yandex.ru>
2019-05-19 08:22:32 +0300

    #33691: add view for getting categories list

--- bikes/bikes_site/urls.py
+++ bikes/bikes_site/urls.py
@@ -1,8 +1,25 @@
-from .views import ProductViewSet, PublicProductViewSet
-from rest_framework import routers
+from django.urls import include, path
+from .views import ProductViewSet, PublicProductViewSet, CategoriesViewSet
 
-router = routers.DefaultRouter()
-router.register('company/products', ProductViewSet, basename='product')
-router.register('products', PublicProductViewSet, basename='public-product')
+user_routes = [
+    path('', PublicProductViewSet.as_view({'get': 'list'})),
+    path('<int:product_id>/', PublicProductViewSet.as_view({'get': 'retrieve'})),
+    path('category/<int:category_id>/', PublicProductViewSet.as_view({'get': 'list'})),
+]
 
-urlpatterns = router.urls
+manager_routes = [
+    path('', ProductViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path(
+        '<int:product_id>/',
+        ProductViewSet.as_view(
+            {'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}
+        ),
+    ),
+    path('category/<int:category_id>/', ProductViewSet.as_view({'get': 'list'})),
+]
+
+urlpatterns = [
+    path('products/', include(user_routes)),
+    path('company/products/', include(manager_routes)),
+    path('categories/', CategoriesViewSet.as_view({'get': 'list'})),
+]
--- bikes/bikes_site/views.py
+++ bikes/bikes_site/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets, permissions
-from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
@@ -9,11 +8,20 @@ from .serializers import (
     CategoryProductSerializer,
     PublicProductSerializer,
     PublicCategoryProductSerializer,
+    CategorySerializer,
 )
 
 
 class PublicProductListPagination(PageNumberPagination):
     page_size = 5
+
+
+class CategoriesViewSet(viewsets.ModelViewSet):
+    """Allows to get a list of all existing categories."""
+
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -24,56 +32,34 @@ class ProductViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         return (
             CategoryProductSerializer
-            if self.action == 'category'
+            if self.kwargs.get('category_id') is not None
             else ProductSerializer
         )
 
     def get_queryset(self):
-        """Collects all products for manager's company.
-
-        Returns
-        -------
-            Products queryset.
-
-        """
-
         manager = Manager.objects.get(user=self.request.user)
-        products = Product.objects.filter(company=manager.company)
-        return products
+        company_products = Product.objects.filter(company=manager.company)
+
+        category_id = self.kwargs.get('category_id')
+        if category_id is not None:
+            category = get_object_or_404(Category, id=category_id)
+            category_products = company_products.filter(category=category)
+            return category_products
+
+        return company_products
+
+    def get_object(self):
+        product_id = self.kwargs['product_id']
+        product = get_object_or_404(self.get_queryset(), id=product_id)
+        return product
 
     def perform_create(self, serializer):
-        """Creates a product instance from manager's company.
-
-        Parameters
-        ----------
-        serializer
-
-        Returns
-        -------
-            Product instance with valid data.
-
-        """
-
         category_id = self.request.data['category']
         category = get_object_or_404(Category, id=category_id)
         manager = Manager.objects.get(user=self.request.user)
         serializer.save(company=manager.company, category=category)
 
     def update(self, request, *args, **kwargs):
-        """Modifies product instance fully or partially.
-
-        Parameters
-        ----------
-        request
-        args
-        kwargs
-
-        Returns
-        -------
-            Product's modified data.
-
-        """
-
         product = self.get_object()
         fields_to_update = request.data
         for field, value in fields_to_update.items():
@@ -82,65 +68,30 @@ class ProductViewSet(viewsets.ModelViewSet):
         updated_product = self.get_serializer(product).data
         return Response(updated_product)
 
-    @action(methods=['get'], detail=False, url_path='category/(?P<pk>[^/.]+)')
-    def category(self, request, pk):
-        """Nested route to get list of company's products for a particular category.
-
-        Parameters
-        ----------
-        request
-        pk
-            Category id.
-
-        Returns
-        -------
-            Products list.
-
-        """
-
-        category = get_object_or_404(Category, id=pk)
-        category_products = self.get_queryset().filter(category=category)
-        category_products_list = [
-            self.get_serializer(item).data for item in category_products
-        ]
-        return Response(category_products_list)
-
 
 class PublicProductViewSet(viewsets.ModelViewSet):
     """Defines actions and queries accessible to all users."""
 
-    http_method_names = ['get']
     permission_classes = (permissions.AllowAny,)
     pagination_class = PublicProductListPagination
-    queryset = Product.objects.all()
+
+    def get_object(self):
+        product_id = self.kwargs['product_id']
+        product = get_object_or_404(Product, id=product_id)
+        return product
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('category_id')
+        if category_id is not None:
+            category = get_object_or_404(Category, id=category_id)
+            category_products = Product.objects.filter(category=category)
+            return category_products
+
+        return Product.objects.all()
 
     def get_serializer_class(self):
         return (
             PublicCategoryProductSerializer
-            if self.action == 'category'
+            if self.kwargs.get('category_id') is not None
             else PublicProductSerializer
         )
-
-    @action(methods=['get'], detail=False, url_path='category/(?P<pk>[^/.]+)')
-    def category(self, request, pk):
-        """Nested route to get list of all products for a particular category.
-
-        Parameters
-        ----------
-        request
-        pk
-            Category id.
-
-        Returns
-        -------
-            Paginated products list.
-
-        """
-
-        category = get_object_or_404(Category, id=pk)
-        category_products = self.get_queryset().filter(category=category)
-        products_per_page = self.paginate_queryset(category_products)
-        category_products_list = [
-            self.get_serializer(item).data for item in products_per_page
-        ]
-        return self.get_paginated_response(category_products_list)
