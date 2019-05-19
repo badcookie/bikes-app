1fefb899cbb6ecfc65f1b624a6cbabf91412b53a
polina.novokreschenova <noverianhenon@yandex.ru>
2019-05-19 09:04:52 +0300

    #33691: add swagger route

--- bikes/bikes_site/urls.py
+++ bikes/bikes_site/urls.py
@@ -1,5 +1,23 @@
 from django.urls import include, path
 from .views import ProductViewSet, PublicProductViewSet, CategoriesViewSet
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 user_routes = [
     path('', PublicProductViewSet.as_view({'get': 'list'})),
@@ -22,4 +40,9 @@ urlpatterns = [
     path('products/', include(user_routes)),
     path('company/products/', include(manager_routes)),
     path('categories/', CategoriesViewSet.as_view({'get': 'list'})),
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui',
+    ),
 ]
